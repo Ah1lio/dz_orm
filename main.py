5e93d97f402e0fe4371592565a4d492a0ef4211f
Egor <egor.lenskiy@mail.ru>
2023-11-18 13:21:49 +1000

    second commit

--- main.py
+++ main.py
@@ -33,14 +33,29 @@ for line in data:
 
 session.commit()
 
-publ_name = input('Ведите имя писателя или id для вывода: ')
-if publ_name.isnumeric():
-    for c in session.query(Publisher).filter(
-            Publisher.id == int(publ_name)).all():
-        print(c)
-else:
-    for c in session.query(Publisher).filter(
-            Publisher.name.like(f'%{publ_name}%')).all():
-        print(c)
+def searching_publisher_name():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите имя (name) издателя: ')
+    query_result = query_join.filter(Publisher.publisher_name == query_publisher_name)
+    for result in query_result.all():
+        print(f'Издатель "{query_publisher_name}" найден в магазине "{result.name}" с идентификатором {result.id}')
+
+
+def searching_publisher_id():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите идентификатор (id) издателя: ')
+    query_result = query_join.filter(Publisher.id_publisher == query_publisher_name)
+    for result in query_result.all():
+        print(
+            f'Издатель c id: {query_publisher_name} найден в магазине "{result.name}" '
+            f'с идентификатором {result.id}')
+
+
+if __name__ == '__main__':
+    searching_publisher_name()
+    searching_publisher_id()
+
+
+session.close()
 
 session.close()
